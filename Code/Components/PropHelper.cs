7cc8de0b18fd543ee29d0d72ded4e21f2769e2b3
Chilevec <59456223+Chilevec@users.noreply.github.com>
2024-12-26 10:42:06 -0800

    fixing NRE
    title

--- Code/Components/PropHelper.cs
+++ Code/Components/PropHelper.cs
@@ -28,6 +28,7 @@ public sealed class PropHelper : Component, Component.ICollisionListener
 	protected override void OnStart()
 	{
 		Prop ??= GetComponent<Prop>();
+		if ( !Prop.IsValid() ) return;
 		Prop.OnPropBreak += OnBreak;
 
 		ModelPhysics ??= GetComponent<ModelPhysics>();
