1a2ce511d25b4d384fae098f0da641fae6f0ea70
trende2001 <68961063+trende2001@users.noreply.github.com>
2025-05-30 22:39:23 +0200

    Move Particle spawning to gameobject prefabs

--- Code/Utils/Particles.cs
+++ Code/Utils/Particles.cs
@@ -1,6 +1,6 @@
 ﻿public static class Particles
 {
-	public static LegacyParticleSystem CreateParticleSystem( string path, Transform transform, float time = 1, GameObject parent = null )
+	public static GameObject CreateParticleSystem( string path, Transform transform, float time = 1, GameObject parent = null )
 	{
 		SpawnParticleSystem( Connection.Local.Id, path, transform.Position, transform.Rotation, time, parent );
 
@@ -16,31 +16,27 @@
 		MakeParticleSystem( path, new Transform( position, rotation ), time, parent );
 	}
 
-	public static LegacyParticleSystem MakeParticleSystem( string path, Transform transform, float time = 1, GameObject parent = null )
+	
+	public static GameObject MakeParticleSystem( string path, Transform transform, float time = 1, GameObject parent = null )
 	{
-		var particleSystem = ParticleSystem.Load( path );
+		var particleSystem = GameObject.GetPrefab( path );
 
 		if ( !particleSystem.IsValid() )
 			return null;
 
-		var go = new GameObject
+		var go = particleSystem.Clone( new CloneConfig
 		{
-			Name = particleSystem.Name,
-			Parent = parent,
-			WorldTransform = transform,
-			NetworkMode = NetworkMode.Never
-		};
+			Name = particleSystem.Name, 
+			Parent = parent, 
+			Transform = transform,
+			StartEnabled = true
+		} );
 
-		var legacyParticleSystem = go.AddComponent<LegacyParticleSystem>();
-		legacyParticleSystem.Particles = particleSystem;
-		legacyParticleSystem.ControlPoints = new()
-		{
-			new ParticleControlPoint { GameObjectValue = go, Value = ParticleControlPoint.ControlPointValueInput.GameObject }
-		};
+		go.NetworkMode = NetworkMode.Never;
 
 		if ( time > 0 )
 			go.DestroyAsync( time );
 
-		return legacyParticleSystem;
+		return go;
 	}
 }
